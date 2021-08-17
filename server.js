13cb62ebf79a821b0275a8c321c74424462ba54c
vikram823 <deshmukhvikram927@gmail.com>
2021-08-18 01:46:12 +0530

    1

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT= process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
